03abe5d4c30b3d15fdf166e9d89b4c52c4bcfe4c
Quang Hung <hungpham307@gmail.com>
2023-08-16 22:48:57 +0700

    setup nativewind

--- babel.config.js
+++ babel.config.js
@@ -3,8 +3,9 @@ module.exports = function (api) {
   return {
     presets: ['babel-preset-expo'],
     plugins: [
+      "nativewind/babel",
       // Required for expo-router
-      'expo-router/babel',
+      require.resolve("expo-router/babel")
     ],
   };
 };
