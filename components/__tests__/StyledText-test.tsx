8e28d2bee7c0d6cec4e54f6e6fde6739aaa9320e
Quang Hung <hungpham307@gmail.com>
2023-08-22 00:25:57 +0700

    chore: delete redundant files

--- components/__tests__/StyledText-test.tsx
+++
@@ -1,7 +0,0 @@
-import { render, screen } from "@testing-library/react-native";
-import { MonoText } from "../StyledText";
-
-it(`renders correctly`, () => {
-  render(<MonoText>Hello</MonoText>);
-  expect(screen.getByText("Hello")).toBeTruthy();
-});
