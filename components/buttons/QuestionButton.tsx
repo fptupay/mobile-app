0f05f71c02a09ef2c16ba3b07a26637101bd486a
Quang Hung <hungpham307@gmail.com>
2023-08-31 21:26:40 +0700

    chore: modify button prop type

--- components/buttons/QuestionButton.tsx
+++ components/buttons/QuestionButton.tsx
@@ -5,7 +5,7 @@ import CustomIcon from "../Icon";
 export default function QuestionButton({ href }: { href: any }) {
   return (
     <Link href={href} className="absolute top-12 z-10 right-4">
-      <CustomIcon name="HelpCircle" color="gray" size="24" />
+      <CustomIcon name="HelpCircle" color="gray" size={30} />
     </Link>
   );
 }
