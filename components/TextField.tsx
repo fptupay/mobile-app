0e98c20e92b6ddea6f921d091548902bbbe3390a
Quang Hung <hungpham307@gmail.com>
2023-09-07 09:42:10 +0700

    refactor: build separate text field component

---
+++ components/TextField.tsx
@@ -0,0 +1,86 @@
+import React, { useEffect, useRef, useState } from "react";
+import {
+  Animated,
+  Easing,
+  Text,
+  TextInput,
+  TouchableWithoutFeedback,
+  View,
+} from "react-native";
+
+interface TextFieldProps extends React.ComponentProps<typeof TextInput> {
+  label: string;
+  errorText?: string | null;
+}
+
+export default function TextField(props: TextFieldProps) {
+  const { label, errorText, value, style, onBlur, onFocus, ...otherProps } =
+    props;
+  const [isFocused, setIsFocused] = useState(false);
+
+  const inputRef = useRef<TextInput>(null);
+  const focusAnim = useRef(new Animated.Value(0)).current;
+
+  useEffect(() => {
+    Animated.timing(focusAnim, {
+      toValue: isFocused || !!value ? 1 : 0,
+      duration: 150,
+      easing: Easing.bezier(0.4, 0, 0.2, 1),
+      useNativeDriver: true,
+    }).start();
+  }, [focusAnim, isFocused, value]);
+
+  return (
+    <View style={style}>
+      <TextInput
+        className="p-4 rounded-lg border border-gray-300 focus:border-primary"
+        ref={inputRef}
+        {...otherProps}
+        value={value}
+        onBlur={(event) => {
+          setIsFocused(false);
+          onBlur?.(event);
+        }}
+        onFocus={(event) => {
+          setIsFocused(true);
+          onFocus?.(event);
+        }}
+      />
+      <TouchableWithoutFeedback onPress={() => inputRef.current?.focus()}>
+        <Animated.View
+          style={{
+            transform: [
+              {
+                scale: focusAnim.interpolate({
+                  inputRange: [0, 1],
+                  outputRange: [1, 0.75],
+                }),
+              },
+              {
+                translateY: focusAnim.interpolate({
+                  inputRange: [0, 1],
+                  outputRange: [20, -12],
+                }),
+              },
+              {
+                translateX: focusAnim.interpolate({
+                  inputRange: [0, 1],
+                  outputRange: [16, 0],
+                }),
+              },
+            ],
+          }}
+          className="absolute px-2 bg-white"
+        >
+          <Text className="text-sm text-gray-300 focus:text-primary">
+            {label}
+            {errorText ? "*" : ""}
+          </Text>
+        </Animated.View>
+      </TouchableWithoutFeedback>
+      {!!errorText && (
+        <Text className="mt-1 ml-3 text-sm text-red-700">{errorText}</Text>
+      )}
+    </View>
+  );
+}
