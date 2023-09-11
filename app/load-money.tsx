a05ef9938c9642c20d23297c1fd54124dd29baf2
qnhanhh <caoquynhanh18@gmail.com>
2023-09-11 17:30:05 +0700

    fix: bank list scroll view

--- app/load-money.tsx
+++ app/load-money.tsx
@@ -8,6 +8,7 @@ import {
   FlatList,
   TouchableWithoutFeedback,
   Keyboard,
+  ScrollView,
 } from "react-native";
 import { MediumText, NormalText, SemiText } from "../components/Themed";
 import TextButton, { TextButtonType } from "../components/buttons/TextButton";
@@ -53,73 +54,71 @@ export default function LoadMoney() {
 
   return (
     <SharedLayout href="/(account)" title="Nạp tiền">
-      <View className="pt-5 py-10 bg-transparent  flex flex-col justify-between">
-        <View className="bg-transparent">
-          <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
-            <View className="bg-transparent">
-              <SemiText className="text-secondary">
-                Nạp tiền vào ví FPTU Pay
-              </SemiText>
-              <TextField
-                label="Số tiền trong ví"
-                className="my-5"
-                value="20.567.000đ"
-                editable={false}
-                selectTextOnFocus={false}
-              />
-              <TextField
-                keyboardType="numeric"
-                label="Số tiền cần nạp"
-                editable={true}
-                selectTextOnFocus={true}
-                value={amount}
-                onChangeText={handleAmountInput}
-              />
-            </View>
-          </TouchableWithoutFeedback>
+      <View className="pt-5 py-10 bg-transparent flex flex-col justify-between">
+        <ScrollView
+          showsVerticalScrollIndicator={false}
+          contentContainerStyle={{ paddingBottom: 150 }}
+        >
+          <View className="bg-transparent">
+            <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+              <View className="bg-transparent">
+                <SemiText className="text-secondary">
+                  Nạp tiền vào ví FPTU Pay
+                </SemiText>
+                <TextField
+                  label="Số tiền trong ví"
+                  className="my-5"
+                  value="20.567.000đ"
+                  editable={false}
+                  selectTextOnFocus={false}
+                />
+                <TextField
+                  keyboardType="numeric"
+                  label="Số tiền cần nạp"
+                  editable={true}
+                  selectTextOnFocus={true}
+                  value={amount}
+                  onChangeText={handleAmountInput}
+                />
+              </View>
+            </TouchableWithoutFeedback>
 
-          <View className="py-8 bg-transparent">
-            <SemiText className="text-secondary mb-5">Từ ngân hàng</SemiText>
-            <FlatList
-              className="max-h-[120px] mb-5 border border-gray-300 rounded-lg p-2"
-              data={listBank}
-              keyExtractor={(item) => item.id.toString()}
-              renderItem={(item) => (
+            <View className="py-8 bg-transparent">
+              <SemiText className="text-secondary mb-5">Từ ngân hàng</SemiText>
+              {listBank.map((item) => (
                 <SelectField
-                  label={item.item.label}
-                  description={item.item.description}
+                  key={item.id}
+                  label={item.label}
+                  description={item.description}
                   className="mb-5"
                 />
-              )}
-            />
-            <IconButton
-              label="Thêm ngân hàng"
-              description="Miễn phí nạp, rút tiền"
-              href="/add-bank"
-            />
+              ))}
+              <IconButton
+                label="Thêm ngân hàng"
+                description="Miễn phí nạp, rút tiền"
+                href="/add-bank"
+              />
+            </View>
           </View>
+        </ScrollView>
+      </View>
+      <View className="bg-white p-4 pb-10 shadow-sm shadow-primary absolute right-0 left-0 bottom-0">
+        <View className="bg-transparent flex flex-row gap-x-2 items-center mb-4">
+          <Image
+            source={require("../assets/images/tick.png")}
+            className="w-6 h-6"
+          />
+          <NormalText className="text-tertiary flex-1 text-xs">
+            Mọi thông tin đều được mã hóa để bảo mật thông tin sinh viên.{" "}
+            <NormalText className="text-primary">Tìm hiểu thêm</NormalText>
+          </NormalText>
         </View>
-
-        <View className="bg-transparent">
-          <View className="bg-transparent flex flex-row gap-x-2 items-center mb-4">
-            <Image
-              source={require("../assets/images/tick.png")}
-              className="w-6 h-6"
-            />
-            <NormalText className="text-tertiary flex-1 text-xs">
-              Mọi thông tin đều được mã hóa để bảo mật thông tin sinh viên.{" "}
-              <NormalText className="text-primary">Tìm hiểu thêm</NormalText>
-            </NormalText>
-          </View>
-          <TouchableOpacity>
-            <TextButton
-              text="Thanh toán"
-              type={TextButtonType.PRIMARY}
-              href="(main-features)/add-bank-item"
-              disable={selectedBank == "" || amount == ""}
-            />
-          </TouchableOpacity>
-        </View>
+        <TextButton
+          text="Thanh toán"
+          type={TextButtonType.PRIMARY}
+          href="(main-features)/add-bank-item"
+          disable={selectedBank == "" || amount == ""}
+        />
       </View>
       {setDepositSuccessful && (
         <Modal
