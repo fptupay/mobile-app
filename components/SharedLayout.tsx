397fed3b950a50bd7b1381c150386f3b958a26f8
Quang Hung <hungpham307@gmail.com>
2023-09-13 17:04:33 +0700

    fix: modify upper part and lower part's height based on dimension

--- components/SharedLayout.tsx
+++ components/SharedLayout.tsx
@@ -2,6 +2,7 @@ import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+import { WINDOW_HEIGHT } from "../utils/helper";
 import GradientBackground from "./GradientBackground";
 import { MediumText } from "./Themed";
 import BackButton from "./buttons/BackButton";
@@ -21,19 +22,23 @@ export default function SharedLayout({
   return (
     <View className="flex-1">
       <StatusBar style="auto" />
-      <View className="h-1/4">
+      <View style={{ height: WINDOW_HEIGHT * 0.25 }}>
         <GradientBackground />
         <SafeAreaView className="px-4 pt-4">
           <BackButton href={href} />
           <QuestionButton href="" />
           <MediumText
-            className={`text-2xl text-secondary mt-[calc(100vh-96vh)]`}
+            className="text-2xl text-secondary"
+            style={{ marginTop: WINDOW_HEIGHT * 0.07 }}
           >
             {title}
           </MediumText>
         </SafeAreaView>
       </View>
-      <View className="absolute top-[calc(100vh-82vh)] left-0 right-0 bottom-0 bg-white flex-1 px-4 rounded-t-[30px] flex justify-between">
+      <View
+        className="absolute left-0 right-0 bottom-0 bg-white flex-1 px-4 rounded-t-[30px] flex justify-between"
+        style={{ top: WINDOW_HEIGHT * 0.2 }}
+      >
         {children}
       </View>
     </View>
