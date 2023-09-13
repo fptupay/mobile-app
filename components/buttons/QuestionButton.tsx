cb95e2015902e61b49385c0ec5e8a93fee70e1b7
Quang Hung <hungpham307@gmail.com>
2023-09-13 17:21:23 +0700

    style: modify button's position

--- components/buttons/QuestionButton.tsx
+++ components/buttons/QuestionButton.tsx
@@ -4,7 +4,7 @@ import CustomIcon from "../Icon";
 
 export default function QuestionButton({ href }: { href: any }) {
   return (
-    <Link href={href} className="absolute top-14 z-10 right-4">
+    <Link href={href} className="z-10 right-4">
       <CustomIcon name="HelpCircle" color="gray" size={30} />
     </Link>
   );
