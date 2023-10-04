9f2e4c2b200f2240a9d50b2293a3c6ca8b7052ea
Quang Hung <hungpham307@gmail.com>
2023-10-04 21:43:11 +0700

    feat: handle client-side validation with request form

--- app/create-request.tsx
+++ app/create-request.tsx
@@ -1,8 +1,15 @@
 import SharedLayout from '@/components/SharedLayout'
+import { NormalText } from '@/components/Themed'
 import TextButton from '@/components/buttons/TextButton'
 import Colors from '@/constants/Colors'
+import {
+  SupportRequestSchema,
+  supportRequestSchema
+} from '@/schemas/request-schema'
+import { zodResolver } from '@hookform/resolvers/zod'
 import React, { useState } from 'react'
-import { KeyboardAvoidingView, Platform, TextInput } from 'react-native'
+import { Controller, useForm } from 'react-hook-form'
+import { KeyboardAvoidingView, Platform, TextInput, View } from 'react-native'
 import DropDownPicker from 'react-native-dropdown-picker'
 
 export default function CreateRequestScreen() {
@@ -13,35 +20,78 @@ export default function CreateRequestScreen() {
     { label: 'Chuyển nhầm', value: 'wrong-transfer' },
     { label: 'Cách sử dụng', value: 'usage' }
   ])
+  const [requestError, setRequestError] = useState('')
+
+  const {
+    control,
+    handleSubmit,
+    formState: { errors }
+  } = useForm<SupportRequestSchema>({
+    defaultValues: {
+      content: ''
+    },
+    resolver: zodResolver(supportRequestSchema),
+    mode: 'onBlur'
+  })
+
+  const onSubmit = (data: SupportRequestSchema) => {
+    console.log(data)
+    if (value === null) {
+      setRequestError('Vui lòng chọn loại yêu cầu')
+    }
+  }
 
   return (
     <SharedLayout href="/index" title="Tạo yêu cầu">
       <KeyboardAvoidingView
         behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
-        className="mt-8 space-y-6 flex-1"
+        className="mt-8 flex-1"
       >
         <DropDownPicker
           open={open}
           placeholder="Chọn loại yêu cầu"
           value={value}
           items={requestType}
+          onChangeValue={() => setRequestError('')}
           setOpen={setOpen}
           setValue={setValue}
           setItems={setRequestType}
           style={{ borderColor: Colors.tertiary }}
-          labelStyle={{ color: Colors.tertiary }}
           textStyle={{ color: Colors.tertiary }}
         />
-        <TextInput
-          editable
-          placeholder="Nhập nội dung yêu cầu"
-          multiline
-          numberOfLines={10}
-          className="border border-gray-200 rounded-lg px-3 py-4 mb-8"
-          textAlignVertical="top"
-        />
+        {requestError ? (
+          <NormalText className="text-red-500 mt-2">{requestError}</NormalText>
+        ) : null}
 
-        <TextButton text="Gửi đơn" type="primary" />
+        <Controller
+          control={control}
+          name="content"
+          render={({ field: { onChange, onBlur, value } }) => (
+            <TextInput
+              placeholder="Nhập nội dung yêu cầu"
+              multiline
+              numberOfLines={10}
+              className="border border-gray-200 rounded-lg px-3 py-4 mt-6 mb-2"
+              textAlignVertical="top"
+              value={value}
+              onChangeText={onChange}
+              onBlur={onBlur}
+            />
+          )}
+        />
+        {errors.content ? (
+          <NormalText className="text-red-500">
+            {errors.content.message}
+          </NormalText>
+        ) : null}
+
+        <View className="mt-auto mb-4">
+          <TextButton
+            onPress={handleSubmit(onSubmit)}
+            text="Gửi đơn"
+            type="primary"
+          />
+        </View>
       </KeyboardAvoidingView>
     </SharedLayout>
   )
