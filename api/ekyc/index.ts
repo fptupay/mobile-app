ca87a0331dede77b1ed7eb36e245c1bb8fea51fd
qnhanhh <caoquynhanh18@gmail.com>
2023-10-04 22:47:01 +0700

    update

--- api/ekyc/index.ts
+++ api/ekyc/index.ts
@@ -6,17 +6,18 @@ export const ekycFront = async (data: string) => {
   const token = await getToken('access_token')
 
   const binaryData = base64.decode(data)
-  const arrayBuffer = new ArrayBuffer(binaryData.length)
-  const uint8Array = new Uint8Array(arrayBuffer)
+  let n = binaryData.length
+  const uint8Array = new Uint8Array(n)
 
-  for (let i = 0; i < binaryData.length; i++) {
-    uint8Array[i] = binaryData.charCodeAt(i)
+  while (n) {
+    uint8Array[n - 1] = binaryData.charCodeAt(n - 1)
+    n -= 1
   }
 
-  const file = new File([uint8Array], 'image.jpg', { type: 'image/jpg' })
+  // const file = new File([uint8Array], 'image.jpg', { type: 'image/jpg' })
 
   const formData = new FormData()
-  formData.append('image', file)
+  formData.append('image', data)
 
   const response = await apiPostCall(
     '/user/ekyc/recognition-id/front',
