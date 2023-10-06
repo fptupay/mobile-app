479bb15e8d76e502fb049e5dc82334b44d779a23
qnhanhh <caoquynhanh18@gmail.com>
2023-10-06 15:31:27 +0700

    ui: alert modal

--- app/transfer-amount.tsx
+++ app/transfer-amount.tsx
@@ -1,7 +1,9 @@
+import PromptModal from '@/components/PromptModal'
 import SharedLayout from '@/components/SharedLayout'
 import TextField from '@/components/TextField'
 import { MediumText, NormalText } from '@/components/Themed'
 import TextButton from '@/components/buttons/TextButton'
+import { useModalStore } from '@/stores/modalStore'
 import { formatMoney } from '@/utils/helper'
 
 import React, { useState } from 'react'
@@ -19,6 +21,9 @@ export default function TransferAmountScreen() {
   const [amount, setAmount] = useState<string>()
   const [suggestions, setSuggestions] = useState<number[]>([])
   const [message, setMessage] = useState<string>('')
+
+  const isOpen = useModalStore((state) => state.isOpen)
+  const setIsOpen = useModalStore((state) => state.setIsOpen)
 
   const handleAmountChange = (amount: string) => {
     // amount should not start with 0
@@ -56,57 +61,65 @@ export default function TransferAmountScreen() {
     setAmount(formatMoney(suggestion))
   }
 
-  return (
-    <SharedLayout href="/transfer" title="Chuyển tiền">
-      <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
-        <KeyboardAvoidingView
-          className="flex-1"
-          behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
-        >
-          {/* Recipient info */}
-          <View className="border border-gray-300 rounded-lg px-4 py-2 flex flex-row justify-between items-center mt-4">
-            <View>
-              <MediumText className="text-black">Phạm Quang Hưng</MediumText>
-              <NormalText className="text-tertiary">HE160005</NormalText>
-            </View>
-            <MediumText className="text-primary">Thay đổi</MediumText>
-          </View>
+  const openModal = () => {
+    setIsOpen(true)
+  }
 
-          {/* Entered amount */}
-          <View className="flex-1 flex-row justify-center items-center">
-            <TextInput
-              className="text-4xl font-semibold text-primary w-full text-center"
-              placeholder="0đ"
-              value={amount}
-              onChangeText={handleAmountChange}
-              keyboardType="numeric"
+  return (
+    <>
+      {isOpen && <PromptModal />}
+      <SharedLayout href="/transfer" title="Chuyển tiền">
+        <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+          <KeyboardAvoidingView
+            className="flex-1"
+            behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+          >
+            {/* Recipient info */}
+            <View className="border border-gray-300 rounded-lg px-4 py-2 flex flex-row justify-between items-center mt-4">
+              <View>
+                <MediumText className="text-black">Phạm Quang Hưng</MediumText>
+                <NormalText className="text-tertiary">HE160005</NormalText>
+              </View>
+              <MediumText className="text-primary">Thay đổi</MediumText>
+            </View>
+
+            {/* Entered amount */}
+            <View className="flex-1 flex-row justify-center items-center">
+              <TextInput
+                className="text-4xl font-semibold text-primary w-full text-center"
+                placeholder="0đ"
+                value={amount}
+                onChangeText={handleAmountChange}
+                keyboardType="numeric"
+              />
+            </View>
+            {/* Suggestion */}
+            <View className="space-x-2 flex-row">
+              {suggestions.map((suggestion) => (
+                <TouchableOpacity
+                  key={suggestion}
+                  onPress={() => handleSuggestionPress(suggestion)}
+                  className="flex-wrap p-1 rounded-md bg-orange-100 text-primary"
+                >
+                  <MediumText>{formatMoney(suggestion)}</MediumText>
+                </TouchableOpacity>
+              ))}
+            </View>
+            <TextField
+              value={message}
+              label="Nhắn gửi"
+              onChangeText={(text) => setMessage(text)}
+              className="my-4"
             />
-          </View>
-          {/* Suggestion */}
-          <View className="space-x-2 flex-row">
-            {suggestions.map((suggestion) => (
-              <TouchableOpacity
-                key={suggestion}
-                onPress={() => handleSuggestionPress(suggestion)}
-                className="flex-wrap p-1 rounded-md bg-orange-100 text-primary"
-              >
-                <MediumText>{formatMoney(suggestion)}</MediumText>
-              </TouchableOpacity>
-            ))}
-          </View>
-          <TextField
-            value={message}
-            label="Nhắn gửi"
-            onChangeText={(text) => setMessage(text)}
-            className="my-4"
-          />
-          <TextButton
-            href="/transfer-confirmation"
-            text="Chuyển tiền"
-            type="primary"
-          />
-        </KeyboardAvoidingView>
-      </TouchableWithoutFeedback>
-    </SharedLayout>
+            <TextButton
+              onPress={openModal}
+              // href="/transfer-confirmation"
+              text="Chuyển tiền"
+              type="primary"
+            />
+          </KeyboardAvoidingView>
+        </TouchableWithoutFeedback>
+      </SharedLayout>
+    </>
   )
 }
