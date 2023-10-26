99341da9d365e0fd1da3d4feddb59b78ad93d63b
pqhung3007 <hungpham307@gmail.com>
2023-10-26 22:58:47 +0700

    feat: form secret key

--- utils/helper.ts
+++ utils/helper.ts
@@ -228,3 +228,11 @@ export const getDeviceId = async () => {
   }
   return Application.androidId
 }
+
+export const generateKey = () => {
+  const array = new Uint8Array(32)
+  window.crypto.getRandomValues(array)
+  return Array.from(array, (byte) => ('0' + byte.toString(16)).slice(-2)).join(
+    ''
+  )
+}
