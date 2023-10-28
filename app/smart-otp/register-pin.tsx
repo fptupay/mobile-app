f3f76f80b268d79a2b630e6892a5d96e3289a9ae
pqhung3007 <hungpham307@gmail.com>
2023-10-29 00:04:11 +0700

    wip (logic): register smart OTP

--- app/smart-otp/register-pin.tsx
+++ app/smart-otp/register-pin.tsx
@@ -1,21 +1,83 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { MediumText, NormalText, SafeAreaView } from '@/components/Themed'
 import { OtpInput } from '@/components/OtpInput'
 import TextButton from '@/components/buttons/TextButton'
+import { router } from 'expo-router'
+import * as SecureStore from 'expo-secure-store'
+import Toast from 'react-native-toast-message'
+import { Pressable } from 'react-native'
+import { OtpInputRef } from '@/types/OtpInput.type'
+import { generateOTP } from '@/api/otp'
 
 export default function RegisterPINScreen() {
+  const [pin, setPin] = useState<string>('')
+  const [confirmedPin, setConfirmedPin] = useState<string>('')
+  const [isPinConfirmed, setIsPinConfirmed] = useState<boolean>(false)
+  const otpRef = useRef<OtpInputRef>(null)
+  const confirmedOtpRef = useRef<OtpInputRef>(null)
+
+  useEffect(() => {
+    if (pin.length !== 6 && confirmedPin.length !== 6) {
+      setIsPinConfirmed(true)
+    }
+  }, [pin, confirmedPin])
+
+  const handleClearPIN = () => {
+    otpRef.current?.clear()
+    setPin('')
+  }
+
+  const handleClearConfirmedPIN = () => {
+    confirmedOtpRef.current?.clear()
+    setConfirmedPin('')
+  }
+
+  const handleSetPIN = async () => {
+    if (pin !== confirmedPin) {
+      Toast.show({
+        type: 'error',
+        text1: 'Mã PIN không khớp',
+        text2: 'Vui lòng nhập lại'
+      })
+      return
+    }
+    // save this pin to local storage
+    await SecureStore.setItemAsync('pin', pin)
+    await generateOTP()
+    router.push('/smart-otp/smart-otp-confirmation')
+  }
+
   return (
     <SafeAreaView>
       <MediumText>Thiết lập mã PIN</MediumText>
       <NormalText>Vui lòng cài đặt PIN cho Smart OTP</NormalText>
 
       <NormalText>Nhập mã PIN</NormalText>
-      <OtpInput numberOfDigits={6} />
+      <OtpInput
+        ref={otpRef}
+        numberOfDigits={6}
+        onTextChange={(text) => setPin(text)}
+      />
+      <Pressable className="mb-5" onPress={handleClearPIN}>
+        <NormalText className="text-primary text-center">Xóa</NormalText>
+      </Pressable>
 
       <NormalText>Nhập lại mã PIN</NormalText>
-      <OtpInput numberOfDigits={6} />
+      <OtpInput
+        ref={confirmedOtpRef}
+        numberOfDigits={6}
+        onTextChange={(text) => setConfirmedPin(text)}
+      />
+      <Pressable className="mb-5" onPress={handleClearConfirmedPIN}>
+        <NormalText className="text-primary text-center">Xóa</NormalText>
+      </Pressable>
 
-      <TextButton type="primary" text="Tiếp tục" />
+      <TextButton
+        type="primary"
+        text="Tiếp tục"
+        disable={!isPinConfirmed}
+        onPress={handleSetPIN}
+      />
     </SafeAreaView>
   )
 }
--- app/smart-otp/smart-otp-confirmation.tsx
+++ app/smart-otp/smart-otp-confirmation.tsx
@@ -1,10 +1,123 @@
-import React from 'react'
-import { MediumText, SafeAreaView } from '@/components/Themed'
+import { registerSmartOTP } from '@/api/otp'
+import { OtpInput } from '@/components/OtpInput'
+import { MediumText, NormalText } from '@/components/Themed'
+import TextButton, { TextButtonType } from '@/components/buttons/TextButton'
+import { OtpInputRef } from '@/types/OtpInput.type'
+import {
+  generateSharedKey,
+  generateTransactionId,
+  getDeviceId,
+  successResponseStatus
+} from '@/utils/helper'
+import { useMutation } from '@tanstack/react-query'
+import { StatusBar } from 'expo-status-bar'
+import * as SecureStore from 'expo-secure-store'
+import React, { useRef, useState } from 'react'
+import {
+  Keyboard,
+  KeyboardAvoidingView,
+  Platform,
+  Pressable,
+  TouchableWithoutFeedback,
+  View
+} from 'react-native'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import Toast from 'react-native-toast-message'
 
 export default function SmartOTPConfirmationScreen() {
+  const otpInputRef = useRef<OtpInputRef>(null)
+  const [otpCode, setOtpCode] = useState<string>('')
+
+  const handleClear = () => {
+    otpInputRef.current?.clear()
+    setOtpCode('')
+  }
+
+  const registerOTPMutation = useMutation({
+    mutationFn: (data: any) => registerSmartOTP(data),
+    onSuccess: (data) => {
+      console.log(data)
+      if (successResponseStatus(data)) {
+        console.log('success')
+      } else {
+        Toast.show({
+          type: 'error',
+          text1: 'Đã có lỗi xảy ra',
+          text2: data.message
+        })
+      }
+    },
+    onError: (error: Error) => {
+      Toast.show({
+        type: 'error',
+        text1: 'Đã có lỗi xảy ra',
+        text2: error.message
+      })
+    }
+  })
+
+  const handleVerifyOTP = async () => {
+    const deviceId = await getDeviceId()
+    const savedPin = await SecureStore.getItemAsync('pin')
+
+    if (savedPin !== null) {
+      registerOTPMutation.mutate({
+        otp: otpCode,
+        otp_pin: savedPin,
+        device_id: deviceId,
+        app_code: 'fptupay',
+        version: Platform.Version.toString(),
+        shared_key: await generateSharedKey(savedPin, deviceId),
+        trans_id: generateTransactionId()
+      })
+    }
+  }
+
   return (
-    <SafeAreaView>
-      <MediumText>Xác thực cài đặt</MediumText>
+    <SafeAreaView className="flex-1 bg-white">
+      <KeyboardAvoidingView
+        behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+        className="flex-1 px-4"
+      >
+        <StatusBar style="auto" />
+
+        <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+          <View className="flex-1 pt-10 space-y-8">
+            <View>
+              <MediumText className="text-3xl text-left tracking-tighter text-secondary">
+                Xác nhận đăng ký
+              </MediumText>
+              <NormalText className="text-tertiary mt-1">
+                Vui lòng nhập mã OTP vừa được gửi tới số điện thoại của bạn
+              </NormalText>
+            </View>
+
+            {/* OTP 6 digits */}
+            <View>
+              <OtpInput
+                ref={otpInputRef}
+                numberOfDigits={6}
+                focusColor="#F97316"
+                onTextChange={(text: any) => setOtpCode(text)}
+              />
+            </View>
+
+            <View className="w-full mt-8 space-y-2">
+              <Pressable className="mb-5" onPress={handleClear}>
+                <NormalText className="text-primary text-center">
+                  Xóa
+                </NormalText>
+              </Pressable>
+              <TextButton
+                text="Xác nhận"
+                type={TextButtonType.PRIMARY}
+                disable={otpCode.length != 6}
+                onPress={() => handleVerifyOTP()}
+              />
+            </View>
+          </View>
+        </TouchableWithoutFeedback>
+      </KeyboardAvoidingView>
     </SafeAreaView>
   )
 }
