96631cd550e72415781128540328f1dfff105aee
pqhung3007 <hungpham307@gmail.com>
2023-10-29 20:46:31 +0700

    fix register pin screen

--- app/smart-otp/register-pin.tsx
+++ app/smart-otp/register-pin.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from 'react'
+import React, { useRef, useState } from 'react'
 import { MediumText, NormalText, SafeAreaView, View } from '@/components/Themed'
 import { OtpInput } from '@/components/OtpInput'
 import TextButton from '@/components/buttons/TextButton'
@@ -13,15 +13,8 @@ import Colors from '@/constants/Colors'
 export default function RegisterPINScreen() {
   const [pin, setPin] = useState<string>('')
   const [confirmedPin, setConfirmedPin] = useState<string>('')
-  const [isPinConfirmed, setIsPinConfirmed] = useState<boolean>(false)
   const otpRef = useRef<OtpInputRef>(null)
   const confirmedOtpRef = useRef<OtpInputRef>(null)
-
-  useEffect(() => {
-    if (pin.length !== 6 && confirmedPin.length !== 6) {
-      setIsPinConfirmed(true)
-    }
-  }, [pin, confirmedPin])
 
   const handleClearPIN = () => {
     otpRef.current?.clear()
@@ -52,43 +45,49 @@ export default function RegisterPINScreen() {
     <SafeAreaView className="flex-1 px-4">
       <View className="pt-8">
         <MediumText className="text-3xl tracking-tight text-secondary">
-          Xác nhận số điện thoại
+          Cài đặt Smart OTP
         </MediumText>
         <NormalText className="text-tertiary mt-1">
-          Vui lòng xác nhận số điện thoại của bạn để gửi mã OTP
+          Vui lòng cài đặt PIN cho cho Smart OTP
         </NormalText>
       </View>
 
-      <NormalText className="text-center mb-2">Nhập mã PIN</NormalText>
-      <OtpInput
-        ref={otpRef}
-        numberOfDigits={6}
-        onTextChange={(text) => setPin(text)}
-        focusColor={Colors.primary}
-        type="covered"
-      />
-      <Pressable className="mb-5" onPress={handleClearPIN}>
-        <NormalText className="text-primary text-center">Xóa</NormalText>
-      </Pressable>
+      <View className="flex space-y-4 mt-8">
+        <MediumText className="text-center mb-2">Nhâp mã PIN</MediumText>
+        <OtpInput
+          ref={otpRef}
+          numberOfDigits={6}
+          onTextChange={(text) => setPin(text)}
+          focusColor={Colors.primary}
+          type="covered"
+        />
+        <Pressable onPress={handleClearPIN}>
+          <NormalText className="text-primary text-center">Xóa</NormalText>
+        </Pressable>
+      </View>
 
-      <NormalText className="text-center mb-2">Nhập lại mã PIN</NormalText>
-      <OtpInput
-        ref={confirmedOtpRef}
-        numberOfDigits={6}
-        onTextChange={(text) => setConfirmedPin(text)}
-        focusColor={Colors.primary}
-        type="covered"
-      />
-      <Pressable className="mb-5" onPress={handleClearConfirmedPIN}>
-        <NormalText className="text-primary text-center">Xóa</NormalText>
-      </Pressable>
+      <View className="flex space-y-4 mt-8">
+        <MediumText className="text-center mb-2">Nhập lại mã PIN</MediumText>
+        <OtpInput
+          ref={confirmedOtpRef}
+          numberOfDigits={6}
+          onTextChange={(text) => setConfirmedPin(text)}
+          focusColor={Colors.primary}
+          type="covered"
+        />
+        <Pressable onPress={handleClearConfirmedPIN}>
+          <NormalText className="text-primary text-center">Xóa</NormalText>
+        </Pressable>
+      </View>
 
-      <TextButton
-        type="primary"
-        text="Tiếp tục"
-        disable={!isPinConfirmed}
-        onPress={handleSetPIN}
-      />
+      <View className="mt-8">
+        <TextButton
+          type="primary"
+          text="Tiếp tục"
+          disable={pin.length < 6 || confirmedPin.length < 6}
+          onPress={handleSetPIN}
+        />
+      </View>
     </SafeAreaView>
   )
 }
