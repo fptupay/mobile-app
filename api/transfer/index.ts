7d0b5a0ba13cdf7c3074619c8eaef8cc666a1efd
pqhung3007 <hungpham307@gmail.com>
2023-11-04 16:10:48 +0700

    fix: missing API header

--- api/transfer/index.ts
+++ api/transfer/index.ts
@@ -1,4 +1,13 @@
+import { Platform } from 'react-native'
 import { apiPostCall } from '..'
+import { getDeviceId } from '@/utils/helper'
+
+const headerConfig = {
+  'x-client-platform': Platform.OS,
+  'x-client-platform-version': Platform.Version.toString(),
+  'x-client-source-app': 'fptupay',
+  'x-sotp-version': Platform.Version.toString()
+}
 
 export const verifyTransfer = async (data: any) => {
   const response = await apiPostCall('/finance/fund-transfer/verify', data, {
@@ -11,10 +20,14 @@ export const verifyTransfer = async (data: any) => {
 }
 
 export const confirmTransfer = async (data: any) => {
+  const deviceId = await getDeviceId()
   const response = await apiPostCall('/finance/fund-transfer/confirm', data, {
-    baseURL: 'https://gateway.fptupay.tech',
     headers: {
-      'Content-Type': 'application/json'
+      ...headerConfig,
+      'x-client-device-id': deviceId,
+      'Content-Type': 'application/json',
+      'x-sotp-device-id': deviceId,
+      'x-sotp-transaction-id': '946904560410782700'
     }
   })
   return response.data
