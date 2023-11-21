75c8acc692f2c8ed17bb9b811f4ad3a6c3e9f048
pqhung3007 <hungpham307@gmail.com>
2023-11-22 00:28:59 +0700

    add API functions for support request

---
+++ api/help-center/index.ts
@@ -0,0 +1,33 @@
+import { compressImg } from '@/utils/helper'
+import { apiGetCall, apiPostCall } from '..'
+import { Platform } from 'react-native'
+
+export const getSupportRequestList = async () => {
+  const response = await apiGetCall('/user/help-center')
+  return response.data
+}
+
+export const uploadImagesToSupportRequest = async (data: any) => {
+  const image = await compressImg(data)
+
+  const file = {
+    uri: Platform.OS === 'ios' ? image.uri.replace('file://', '') : image.uri,
+    type: 'image/jpeg',
+    name: 'avatar.jpg'
+  }
+
+  const formData = new FormData()
+  formData.append('image', file as any)
+
+  const response = await apiPostCall('/user/avatar/change', formData, {
+    headers: {
+      'Content-Type': 'multipart/form-data'
+    }
+  })
+  return response.data
+}
+
+export const createSupportRequest = async (data: any) => {
+  const response = await apiPostCall('/user/help-center/new-help-center', data)
+  return response.data
+}
