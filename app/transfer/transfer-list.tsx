ff526d93bb06888ecb05f38eee3fa47ee67b93e2
pqhung3007 <hungpham307@gmail.com>
2023-11-27 22:23:47 +0700

    refactor: only keep saved account

--- app/transfer/transfer-list.tsx
+++ app/transfer/transfer-list.tsx
@@ -1,112 +1,20 @@
+import { getSavedAccounts } from '@/api/transfer'
 import CustomIcon from '@/components/Icon'
 import SharedLayout from '@/components/SharedLayout'
 import { MediumText, NormalText, SemiText } from '@/components/Themed'
+import { useQuery } from '@tanstack/react-query'
 
 import { useRouter } from 'expo-router'
-import React, { useState } from 'react'
-import {
-  FlatList,
-  Keyboard,
-  TouchableOpacity,
-  TouchableWithoutFeedback,
-  View
-} from 'react-native'
-
-const transferTypes = ['Tất cả', 'Gần đây', 'Đã lưu']
-const mockTransferList = [
-  {
-    id: 1,
-    name: 'Phạm Quang Hưng',
-    accountNumber: 'HE160001'
-  },
-  {
-    id: 2,
-    name: 'Phạm Quang Hưng',
-    accountNumber: 'HE160002'
-  },
-  {
-    id: 3,
-    name: 'Phạm Quang Hưng',
-    accountNumber: 'HE160003'
-  }
-]
+import React from 'react'
+import { FlatList, TouchableOpacity, View } from 'react-native'
 
 export default function TransferListScreen() {
   const router = useRouter()
-  const [transferType, setTransferType] = useState<string>(transferTypes[0])
 
-  const recentList = () => (
-    <View className="mt-4">
-      <SemiText className="text-secondary">Gần đây</SemiText>
-      <FlatList
-        data={mockTransferList}
-        renderItem={({ item }) => (
-          <TouchableOpacity className="flex flex-row items-center justify-between border-b border-gray-300 py-3">
-            <View className="flex flex-row items-center">
-              <View className="w-10 h-10 bg-gray-300 rounded-full" />
-              <View className="ml-2">
-                <MediumText className="text-secondary">{item.name}</MediumText>
-                <NormalText className="text-tertiary">
-                  {item.accountNumber}
-                </NormalText>
-              </View>
-            </View>
-            <CustomIcon name="MoreHorizontal" size={24} color="#000" />
-          </TouchableOpacity>
-        )}
-        keyExtractor={(item) => item.id.toString()}
-      />
-    </View>
-  )
-
-  const savedList = () => (
-    <View className="mt-4">
-      <SemiText className="text-secondary">Đã lưu</SemiText>
-      <FlatList
-        data={mockTransferList}
-        renderItem={({ item }) => (
-          <TouchableOpacity className="flex flex-row items-center justify-between border-b border-gray-300 py-3">
-            <View className="flex flex-row items-center">
-              <View className="w-10 h-10 bg-gray-300 rounded-full" />
-              <View className="ml-2">
-                <MediumText className="text-secondary">{item.name}</MediumText>
-                <NormalText className="text-tertiary">
-                  {item.accountNumber}
-                </NormalText>
-              </View>
-            </View>
-            <CustomIcon name="MoreHorizontal" size={24} color="#000" />
-          </TouchableOpacity>
-        )}
-        keyExtractor={(item) => item.id.toString()}
-      />
-    </View>
-  )
-
-  const renderList = () => {
-    switch (transferType) {
-      case 'Tất cả':
-        return (
-          <FlatList
-            data={mockTransferList}
-            renderItem={null}
-            ListHeaderComponent={recentList}
-            ListFooterComponent={savedList}
-            showsVerticalScrollIndicator={false}
-          />
-        )
-      case 'Gần đây':
-        return recentList()
-      case 'Đã lưu':
-        return savedList()
-      default:
-        return null
-    }
-  }
-
-  const handleTransferTypeChange = (type: string) => {
-    setTransferType(type)
-  }
+  const { data: friends } = useQuery({
+    queryKey: ['friends'],
+    queryFn: getSavedAccounts
+  })
 
   return (
     <SharedLayout
@@ -125,38 +33,29 @@ export default function TransferListScreen() {
         </MediumText>
       </TouchableOpacity>
 
-      <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
-        <View>
-          <View className="flex flex-row space-x-2 mt-6">
-            <FlatList
-              data={transferTypes}
-              renderItem={({ item }) => (
-                <TouchableOpacity
-                  className={`border rounded-full px-2 py-1 ${
-                    transferType === item ? 'border-primary' : 'border-tertiary'
-                  }`}
-                  onPress={() => handleTransferTypeChange(item)}
-                >
-                  <NormalText
-                    className={`${
-                      transferType === item ? 'text-primary' : 'text-tertiary'
-                    }`}
-                  >
-                    {item}
+      <View className="mt-4">
+        <SemiText className="text-secondary">Đã lưu</SemiText>
+        <FlatList
+          data={friends?.data}
+          renderItem={({ item }) => (
+            <TouchableOpacity className="flex flex-row items-center justify-between border-b border-gray-300 py-3">
+              <View className="flex flex-row items-center">
+                <View className="w-10 h-10 bg-gray-300 rounded-full" />
+                <View className="ml-2">
+                  <MediumText className="text-secondary">
+                    {item.name}
+                  </MediumText>
+                  <NormalText className="text-tertiary">
+                    {item.ref_user_id}
                   </NormalText>
-                </TouchableOpacity>
-              )}
-              showsHorizontalScrollIndicator={false}
-              horizontal={true}
-              keyExtractor={(item) => item}
-              contentContainerStyle={{ columnGap: 10 }}
-            />
-          </View>
-        </View>
-      </TouchableWithoutFeedback>
-
-      {/* list rendered based on transfer types */}
-      {renderList()}
+                </View>
+              </View>
+              <CustomIcon name="MoreHorizontal" size={24} color="#000" />
+            </TouchableOpacity>
+          )}
+          keyExtractor={(item) => item.id.toString()}
+        />
+      </View>
     </SharedLayout>
   )
 }
