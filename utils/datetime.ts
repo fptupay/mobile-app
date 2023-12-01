91d636d43f6f588c18db019595c53afc56335575
pqhung3007 <hungpham307@gmail.com>
2023-12-01 21:27:27 +0700

    fix: date format

--- utils/datetime.ts
+++ utils/datetime.ts
@@ -1,7 +1,8 @@
 export const extractDateStringFromCurrentDate = (date: Date) => {
   const year = date.getFullYear()
-  const month = date.getMonth() + 1
-  const day = date.getDate()
+  const month =
+    date.getMonth() + 1 < 10 ? `0${date.getMonth() + 1}` : date.getMonth() + 1
+  const day = date.getDate() < 10 ? `0${date.getDate()}` : date.getDate()
 
   const dateString = `${year}-${month}-${day}`
 
