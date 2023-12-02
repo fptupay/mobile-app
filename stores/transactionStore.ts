9e09d0b9b652deb81009eb6fab9ad17da7229a8a
pqhung3007 <hungpham307@gmail.com>
2023-12-02 21:48:04 +0700

    refactor: store

--- stores/transactionStore.ts
+++ stores/transactionStore.ts
@@ -3,15 +3,20 @@ import { create } from 'zustand'
 interface TransactionProps {
   fundTransferId: string
   transactionDetails: any
+  accountNumber: string
   setFundTransferId: (fundTransferId: string) => void
   setTransactionDetails: (transactionDetails: any) => void
+  setAccountNumber: (accountNumber: string) => void
 }
 
 export const useTransactionStore = create<TransactionProps>((set) => ({
   fundTransferId: '',
   transactionDetails: {},
+  accountNumber: '',
   setFundTransferId: (fundTransferId: string) =>
     set({ fundTransferId: fundTransferId }),
   setTransactionDetails: (transactionDetails: any) =>
-    set({ transactionDetails: transactionDetails })
+    set({ transactionDetails: transactionDetails }),
+  setAccountNumber: (accountNumber: string) =>
+    set({ accountNumber: accountNumber })
 }))
