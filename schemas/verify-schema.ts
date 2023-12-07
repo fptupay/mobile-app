2f71a5c3091dfd0c9b51bb661bb65917ba501473
pqhung3007 <hungpham307@gmail.com>
2023-12-07 21:49:28 +0700

    fix (reset password): form schema

--- schemas/verify-schema.ts
+++ schemas/verify-schema.ts
@@ -15,9 +15,6 @@ export const infoResetPasswordSchema = z.object({
   email: z.string().email('Email không hợp lệ').trim(),
   card_no: z.string().length(12, 'Số CMND/CCCD phải có 12 ký tự').trim(),
   date_of_birth: z.string().trim(),
-  card_holder_name: z
-    .string()
-    .length(6, 'Tên chủ thẻ phải có ít nhất 6 ký tự')
-    .trim()
+  card_holder_name: z.string().min(1, 'Tên chủ thẻ không được để trống').trim()
 })
 export type InfoSchema = z.infer<typeof infoResetPasswordSchema>
