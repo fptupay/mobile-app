78f965497347ec8ecb8918353dac6d384c64a573
pqhung3007 <hungpham307@gmail.com>
2023-12-13 22:52:00 +0700

    fix(help center): clear old data after submitting

--- app/account/help-center/create-request.tsx
+++ app/account/help-center/create-request.tsx
@@ -83,6 +83,13 @@ export default function CreateRequestScreen() {
           pathname: '/account/help-center/successful-request',
           params: { id: data.data.id }
         })
+        // clear state
+        setValue('')
+        setTransactionId('')
+        setTitle('')
+        setDescription('')
+        setImages([])
+
         await queryClient.invalidateQueries(['requests'])
       } else {
         Toast.show({
@@ -161,7 +168,9 @@ export default function CreateRequestScreen() {
             onChangeText={(text) => setDescription(text)}
           />
 
-          <NormalText className="text-tertiary">Thêm ảnh chứng minh</NormalText>
+          <NormalText className="text-tertiary">
+            Thêm ảnh chứng minh (tuỳ chọn)
+          </NormalText>
           <View className="flex flex-row">
             {images && (
               <View className="flex flex-row flex-wrap">
